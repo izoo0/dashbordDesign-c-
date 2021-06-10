7e0a825b00e1ca789232294ad6c8b17774a5916c
izoo0 <isaiahwainaina34@gmail.com>
2021-06-10 15:25:24 +0300

    Add project files.

---
+++ design1/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace design1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bunifuShadowPanel1_ControlAdded(object sender, ControlEventArgs e)
+        {
+
+        }
+
+        private void bunifuShadowPanel1_ControlAdded_1(object sender, ControlEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuSeparator5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuSeparator2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuSeparator2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuPanel1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
